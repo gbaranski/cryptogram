ffdc42ec316a30ae090a43f1e65b2d075e3ca338
gbaranski <gbaranski19@gmail.com>
2020-11-27 21:51:41 +0100

    fix: fix initailizing database

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:cryptogram/route_generator.dart';
+import 'package:cryptogram/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final Future<Database> database = openDatabase(
-      // Set the path to the database. Note: Using the `join` function from the
-      // `path` package is best practice to ensure the path is correctly
-      // constructed for each platform.
-      join(await getDatabasesPath(), 'database.db'), onCreate: (db, version) {
-    return db.execute(
-        'CREATE TABLE accounts(accountID TINYTEXT PRIMARY KEY, customName TEXT)');
-  }, version: 1);
+  DatabaseService.init();
   runApp(MyApp());
 }
 
