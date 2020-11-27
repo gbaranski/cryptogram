0ff3bd76d7fecfcfe991e29984f4b24d8e239b59
gbaranski <gbaranski19@gmail.com>
2020-11-27 01:08:15 +0100

    feat: Add create account view

--- lib/views/account/create.dart
+++ lib/views/account/create.dart
@@ -1,13 +1,75 @@
+import 'package:cryptogram/views/account/get_password.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:stellar_flutter_sdk/stellar_flutter_sdk.dart' hide Row;
 
-class CreateAccountView extends StatelessWidget {
+class CreateAccountView extends StatefulWidget {
   static const String route = '/account/create';
+
+  @override
+  _CreateAccountViewState createState() => _CreateAccountViewState();
+}
+
+class _CreateAccountViewState extends State<CreateAccountView> {
+  KeyPair _keyPair = KeyPair.random();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Text("Create new wallet"),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          children: [
+            ListTile(
+              leading: Icon(MdiIcons.accountKey),
+              onTap: () {
+                Clipboard.setData(ClipboardData(text: _keyPair.accountId));
+                HapticFeedback.lightImpact();
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text("Copied AccountID to clipboard"),
+                ));
+              },
+              title: Text("Account ID"),
+              subtitle: Text(_keyPair.accountId),
+            ),
+            ListTile(
+              leading: Icon(MdiIcons.keyStar),
+              onTap: () {
+                Clipboard.setData(ClipboardData(text: _keyPair.accountId));
+                HapticFeedback.lightImpact();
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text(
+                      "Copied secret to clipboard, save it in secure place"),
+                ));
+              },
+              title: Text("Secret seed"),
+              subtitle: Text(_keyPair.secretSeed),
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                OutlinedButton(
+                  onPressed: () {
+                    setState(() {
+                      _keyPair = KeyPair.random();
+                    });
+                  },
+                  child: Text("Generate new key pair"),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    print(await getUserPassword(context));
+                  },
+                  child: Text("Continue"),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
