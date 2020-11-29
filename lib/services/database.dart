72a7239073315d7a2f6b3d6083f82a7a679ab44f
gbaranski <gbaranski19@gmail.com>
2020-11-29 22:47:50 +0100

    fix: Remove promise on database

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -3,29 +3,31 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseService {
-  static Future<Database> _database;
+  static Database _database;
 
   static Future<void> init() async {
-    _database = openDatabase(join(await getDatabasesPath(), 'database.db'),
+    print("Executed init");
+    _database = await openDatabase(join(await getDatabasesPath(), 'database.db'),
         onCreate: (db, version) {
       return db.execute(
           'CREATE TABLE accounts(accountID TEXT PRIMARY KEY, secretSeed TEXT, customName TEXT)');
     }, version: 1);
+    print("DB: ${_database}");
   }
 
   static Future<List<Account>> getAccounts() async {
     final List<Map<String, dynamic>> accounts =
-        await (await _database).query('accounts');
+        await _database.query('accounts');
     return accounts.map((account) => Account.fromMap(account)).toList();
   }
 
   static Future<void> addAccount(Account account) async {
-    (await _database).insert('accounts', account.toMap(),
+    _database.insert('accounts', account.toMap(),
         conflictAlgorithm: ConflictAlgorithm.fail);
   }
 
   static Future<void> deleteAccount(Account account) async {
-    final int result = await (await _database).delete('accounts',
+    final int result = await _database.delete('accounts',
         where: "accountID = ? AND customName = ? AND secretSeed = ?",
         whereArgs: [account.accountID, account.customName, account.secretSeed]);
     if (result == 0) throw new Exception('Account not found');
