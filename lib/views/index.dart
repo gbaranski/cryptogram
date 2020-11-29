0c322af0e5b4273991d8a9803bbe8f203661e3fc
gbaranski <gbaranski19@gmail.com>
2020-11-29 22:48:14 +0100

    feat: Add displaying currently saved accounts

--- lib/views/index.dart
+++ lib/views/index.dart
@@ -1,6 +1,8 @@
+import 'package:cryptogram/models/account.dart';
+import 'package:cryptogram/services/database.dart';
+import 'package:cryptogram/views/account/create.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:stellar_flutter_sdk/stellar_flutter_sdk.dart' hide Row;
 
 enum AccountAction {
   delete,
@@ -19,14 +21,56 @@ class _IndexViewState extends State<IndexView> {
 
   @override
   void initState() {
+    DatabaseService.getAccounts().then((accounts) {
+      setState(() => _accounts = accounts);
+    });
     super.initState();
   }
+
+  Widget accountsList(BuildContext context) => SliverList(
+          delegate: SliverChildBuilderDelegate((context, i) {
+        final account = _accounts[i];
+        return ListTile(
+          onTap: () {},
+          leading: Icon(MdiIcons.accountKey),
+          trailing: PopupMenuButton<AccountAction>(
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                  value: AccountAction.delete,
+                  child: ListTile(
+                    visualDensity: VisualDensity.compact,
+                    contentPadding: EdgeInsets.zero,
+                    leading: Icon(Icons.delete),
+                    title: Text("Delete"),
+                  )),
+              PopupMenuItem(
+                  value: AccountAction.edit,
+                  enabled: false,
+                  child: ListTile(
+                    visualDensity: VisualDensity.compact,
+                    contentPadding: EdgeInsets.zero,
+                    leading: Icon(Icons.edit),
+                    title: Text(
+                      "Edit",
+                      style: TextStyle(color: Colors.black38),
+                    ),
+                  )),
+            ],
+          ),
+          title: Text(
+            account.accountID,
+            overflow: TextOverflow.ellipsis,
+          ),
+          subtitle: Text(account.customName),
+        );
+      }, childCount: _accounts.length));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => Navigator.pushNamed(context, CreateAccountView.route),
         child: Icon(MdiIcons.accountPlus),
       ),
       body: CustomScrollView(
@@ -36,44 +80,12 @@ class _IndexViewState extends State<IndexView> {
           SliverAppBar(
             title: Text("Pick account"),
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-                (context, i) => ListTile(
-                      onTap: () {},
-                      leading: Icon(MdiIcons.accountKey),
-                      trailing: PopupMenuButton<AccountAction>(
-                        icon: Icon(Icons.more_vert),
-                        itemBuilder: (context) => [
-                          PopupMenuItem(
-                              value: AccountAction.delete,
-                              child: ListTile(
-                                visualDensity: VisualDensity.compact,
-                                contentPadding: EdgeInsets.zero,
-                                leading: Icon(Icons.delete),
-                                title: Text("Delete"),
-                              )),
-                          PopupMenuItem(
-                              value: AccountAction.edit,
-                              enabled: false,
-                              child: ListTile(
-                                visualDensity: VisualDensity.compact,
-                                contentPadding: EdgeInsets.zero,
-                                leading: Icon(Icons.edit),
-                                title: Text(
-                                  "Edit",
-                                  style: TextStyle(color: Colors.black38),
-                                ),
-                              )),
-                        ],
-                      ),
-                      title: Text(
-                        KeyPair.random().accountId,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      subtitle: Text("Grzegorz"),
-                    ),
-                childCount: 2),
-          )
+          if (_accounts != null)
+            accountsList(context)
+          else
+            (SliverToBoxAdapter(
+              child: CircularProgressIndicator(),
+            ))
         ],
       ),
     );
