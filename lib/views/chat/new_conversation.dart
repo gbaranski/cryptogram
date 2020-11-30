c3f2ebb74f47706bb6309d7f34db205bb4413435
gbaranski <gbaranski19@gmail.com>
2020-11-30 23:18:57 +0100

    feat: Add working sending conversations

--- lib/views/chat/new_conversation.dart
+++ lib/views/chat/new_conversation.dart
@@ -1,8 +1,15 @@
+import 'package:cryptogram/models/account.dart';
+import 'package:cryptogram/services/blockchain.dart';
+import 'package:cryptogram/services/crypto.dart';
+import 'package:cryptogram/views/account/get_password.dart';
 import 'package:flutter/material.dart';
-import 'package:stellar_flutter_sdk/stellar_flutter_sdk.dart' hide Row;
+import 'package:stellar_flutter_sdk/stellar_flutter_sdk.dart' hide Row, Account;
 
 class NewConversationView extends StatefulWidget {
   static const route = '/new_conversation';
+  final Account account;
+  NewConversationView(this.account);
+
   @override
   _NewConversationViewState createState() => _NewConversationViewState();
 }
@@ -10,7 +17,7 @@ class NewConversationView extends StatefulWidget {
 class _NewConversationViewState extends State<NewConversationView> {
   final _formKey = GlobalKey<FormState>();
 
-  String accountID;
+  String destinationAccountID;
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +29,35 @@ class _NewConversationViewState extends State<NewConversationView> {
           autovalidateMode: AutovalidateMode.onUserInteraction,
           child: ListTile(
             trailing: IconButton(
-              onPressed: () {},
+              onPressed: () async {
+                if (!_formKey.currentState.validate()) return;
+                _formKey.currentState.save();
+                final userPassword = await getUserPassword(context);
+                final decryptedSecretSeed = await Crypto.decryptSecretSeed(
+                    widget.account.secretSeed, userPassword);
+                print("Decrypted secret seed: $decryptedSecretSeed");
+                final senderKeyPair =
+                    KeyPair.fromSecretSeed(decryptedSecretSeed);
+
+                final result = await Blockchain.sendMessage(
+                    destination: destinationAccountID,
+                    message: "Hello world",
+                    senderKeypair: senderKeyPair);
+
+                print("Success: ${result.success}");
+                print("Sender: ${widget.account.accountID}");
+                print("Destination: $destinationAccountID");
+              },
               icon: Icon(Icons.send),
             ),
             title: TextFormField(
                 decoration: const InputDecoration(labelText: 'Account ID'),
                 autocorrect: false,
+                onSaved: (String value) {
+                  setState(() {
+                    destinationAccountID = value;
+                  });
+                },
                 validator: (value) {
                   try {
                     if (value == null)
@@ -38,11 +68,6 @@ class _NewConversationViewState extends State<NewConversationView> {
                     return "Invalid: ${e.message.toLowerCase()}";
                   }
                 }),
-
-            //   IconButton(
-            //     onPressed: () {},
-            //     icon: Icon(Icons.arrow_right),
-            //   )
           ),
         ),
       ),
