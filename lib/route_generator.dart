a0e58d6d996d22c43b398d3d10af3dfa4c034613
gbaranski <gbaranski19@gmail.com>
2020-11-30 14:39:19 +0100

    fix: generating routes

--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -9,21 +9,23 @@ import 'package:flutter/material.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
-    return MaterialPageRoute(builder: (context) {
-      switch (settings.name) {
-        case IndexView.route:
-          return IndexView();
-        case ChatView.route:
-          return ChatView();
-        case CreateAccountView.route:
-          return CreateAccountView();
-        case RestoreAccountView.route:
-          return RestoreAccountView();
-        case AccountView.route:
-          return AccountView();
-        default:
-          return ErrorScreen();
-      }
-    });
+    return MaterialPageRoute(
+        builder: (context) {
+          switch (settings.name) {
+            case IndexView.route:
+              return IndexView();
+            case ChatView.route:
+              return ChatView();
+            case CreateAccountView.route:
+              return CreateAccountView();
+            case RestoreAccountView.route:
+              return RestoreAccountView();
+            case AccountView.route:
+              return AccountView();
+            default:
+              return ErrorScreen();
+          }
+        },
+        settings: settings);
   }
 }
