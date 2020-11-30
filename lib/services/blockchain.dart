ec7c5a6c72729c5bf30742de3d5cb1d1ff0d572c
gbaranski <gbaranski19@gmail.com>
2020-11-30 23:18:23 +0100

    feat: add Blockchain.sendMessage

--- lib/services/blockchain.dart
+++ lib/services/blockchain.dart
@@ -1,4 +1,5 @@
 import 'package:cryptogram/models/account.dart';
+import 'package:flutter/material.dart';
 import 'package:stellar_flutter_sdk/stellar_flutter_sdk.dart' hide Account;
 import 'package:url_launcher/url_launcher.dart';
 
@@ -19,5 +20,22 @@ abstract class Blockchain {
     } else {
       throw new Exception('Could not open URL');
     }
+  }
+
+  static Future<SubmitTransactionResponse> sendMessage(
+      {@required KeyPair senderKeypair,
+      @required String destination,
+      @required String message}) async {
+    AccountResponse sender =
+        await _sdk.accounts.account(senderKeypair.accountId);
+    final memo = MemoText(message);
+    Transaction transaction = new TransactionBuilder(sender)
+        .addOperation(
+            PaymentOperationBuilder(destination, Asset.NATIVE, "100").build())
+        .addMemo(memo)
+        .build();
+    transaction.sign(senderKeypair, Network.TESTNET);
+
+    return _sdk.submitTransaction(transaction);
   }
 }
