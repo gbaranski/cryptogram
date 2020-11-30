16a6897289751b454b1820e79e5e9e13bc24c6cb
gbaranski <gbaranski19@gmail.com>
2020-11-30 20:30:34 +0100

    feat: add opening account info on stellarchain

--- lib/services/blockchain.dart
+++ lib/services/blockchain.dart
@@ -1,5 +1,6 @@
 import 'package:cryptogram/models/account.dart';
 import 'package:stellar_flutter_sdk/stellar_flutter_sdk.dart' hide Account;
+import 'package:url_launcher/url_launcher.dart';
 
 abstract class Blockchain {
   static final _sdk = StellarSDK.TESTNET;
@@ -9,4 +10,14 @@ abstract class Blockchain {
 
   static Future<AccountResponse> getAccountInfo(Account account) =>
       _sdk.accounts.account(account.accountID);
+
+  static Future<void> openAccountInfo(Account account) async {
+    final String url =
+        "http://testnet.stellarchain.io/address/${account.accountID}";
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw new Exception('Could not open URL');
+    }
+  }
 }
