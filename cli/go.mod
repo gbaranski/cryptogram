cd3b51c7b8ee83181f6e010b661c9dc900b4a035
gbaranski <gbaranski19@gmail.com>
2020-12-05 10:46:01 +0100

    feat: Add auto completion with fuzzy search

--- cli/go.mod
+++ cli/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/ipfs/go-ipfs v0.7.0
 	github.com/ipfs/go-ipfs-config v0.9.0
 	github.com/ipfs/interface-go-ipfs-core v0.4.0
+	github.com/lithammer/fuzzysearch v1.1.1
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	golang.org/x/sys v0.0.0-20201202213521-69691e467435 // indirect
 )
--- cli/repl/completer.go
+++ cli/repl/completer.go
@@ -4,12 +4,42 @@ import (
 	"strings"
 
 	"github.com/c-bata/go-prompt"
+	"github.com/lithammer/fuzzysearch/fuzzy"
 )
+
+type mathSuggestion struct {
+	promptSuggestion prompt.Suggest
+	score            int
+}
+
+func findSuggestions(str string, availableSuggestions []prompt.Suggest) []prompt.Suggest {
+	var suggestionsStrings []string
+	for _, e := range availableSuggestions {
+		suggestionsStrings = append(suggestionsStrings, e.Text)
+	}
+	foundSuggestionsString := fuzzy.Find(str, suggestionsStrings)
+	var foundSuggestions []prompt.Suggest
+	for _, e := range foundSuggestionsString {
+		for _, c := range availableSuggestions {
+			if e == c.Text {
+				foundSuggestions = append(foundSuggestions, c)
+			}
+
+		}
+
+	}
+	return foundSuggestions
+}
 
 // Completer adds completer feature
 func Completer(d prompt.Document) []prompt.Suggest {
-	if strings.HasPrefix(d.Text, "topic") {
-		return topicSuggestions
+	cmdText := strings.Trim(d.Text, " ")
+	if len(cmdText) == 0 {
+		return commandSuggestions
 	}
-	return commandSuggestions
+	if strings.HasPrefix(cmdText, "topic") {
+		cmdText := strings.Trim(strings.TrimPrefix(cmdText, "topic"), " ")
+		return findSuggestions(cmdText, topicSuggestions)
+	}
+	return findSuggestions(cmdText, commandSuggestions)
 }
