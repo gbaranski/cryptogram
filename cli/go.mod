383f4fd36e9c1fd448045d41998d10ee7504a711
gbaranski <gbaranski19@gmail.com>
2020-12-06 14:46:45 +0100

    feat: add bootstrapper nodes

--- cli/go.mod
+++ cli/go.mod
@@ -7,7 +7,10 @@ require (
 	github.com/ipfs/go-ipfs v0.7.0
 	github.com/ipfs/go-ipfs-config v0.9.0
 	github.com/ipfs/interface-go-ipfs-core v0.4.0
+	github.com/libp2p/go-libp2p-core v0.6.1
+	github.com/libp2p/go-libp2p-peerstore v0.2.6
 	github.com/lithammer/fuzzysearch v1.1.1
 	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/multiformats/go-multiaddr v0.3.1
 	golang.org/x/sys v0.0.0-20201202213521-69691e467435 // indirect
 )
--- cli/node/init.go
+++ cli/node/init.go
@@ -4,9 +4,15 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"path/filepath"
+	"sync"
 
 	config "github.com/ipfs/go-ipfs-config"
+	iface "github.com/ipfs/interface-go-ipfs-core"
+	"github.com/libp2p/go-libp2p-core/peer"
+	peerstore "github.com/libp2p/go-libp2p-peerstore"
+	ma "github.com/multiformats/go-multiaddr"
 
 	"github.com/ipfs/go-ipfs/core"
 	"github.com/ipfs/go-ipfs/core/coreapi"
@@ -89,6 +95,41 @@ func setupPlugins(externalPluginsPath string) error {
 		return fmt.Errorf("error initializing plugins: %s", err)
 	}
 
+	return nil
+}
+
+// ConnectToPeers connect to peers
+func ConnectToPeers(ctx context.Context, ipfs iface.CoreAPI, peers []string) error {
+	var wg sync.WaitGroup
+	peerInfos := make(map[peer.ID]*peerstore.PeerInfo, len(peers))
+	for _, addrStr := range peers {
+		addr, err := ma.NewMultiaddr(addrStr)
+		if err != nil {
+			return err
+		}
+		pii, err := peerstore.InfoFromP2pAddr(addr)
+		if err != nil {
+			return err
+		}
+		pi, ok := peerInfos[pii.ID]
+		if !ok {
+			pi = &peerstore.PeerInfo{ID: pii.ID}
+			peerInfos[pi.ID] = pi
+		}
+		pi.Addrs = append(pi.Addrs, pii.Addrs...)
+	}
+
+	wg.Add(len(peerInfos))
+	for _, peerInfo := range peerInfos {
+		go func(peerInfo *peerstore.PeerInfo) {
+			defer wg.Done()
+			err := ipfs.Swarm().Connect(ctx, *peerInfo)
+			if err != nil {
+				log.Printf("failed to connect to %s: %s", peerInfo.ID, err)
+			}
+		}(peerInfo)
+	}
+	wg.Wait()
 	return nil
 }
 
