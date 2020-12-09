fceb43f4518cbf4612a69eae089704b7c46b4abd
gbaranski <gbaranski19@gmail.com>
2020-12-09 23:38:35 +0100

    feat: Add listening for connecting peers, sending msgs

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,15 +1,33 @@
+import React, { ReactElement } from "react";
+import Libp2p from "libp2p";
 import Head from "next/head";
 import { useState } from "react";
 import { startNode } from "../services/libp2p";
 import { createOrGetPeerID } from "../services/peerid";
 
-export default function Home() {
-  const [libp2p, setLibp2p] = useState<LibP2p | undefined>(undefined);
+export default function Home(): ReactElement {
+  const [libp2p, setLibp2p] = useState<Libp2p | undefined>(undefined);
   const [ID, setID] = useState<string>("Loading...");
 
   const connect = async () => {
     const peerID = await createOrGetPeerID();
+    console.log({ peerID });
+    setID(peerID.toB58String());
     const _libp2p = await startNode(peerID);
+    _libp2p.connectionManager.on("peer:connect", (connection) =>
+      console.log("Connected peer: ", connection)
+    );
+    _libp2p.connectionManager.on("peer:disconnect", (connection) =>
+      console.log("Disconnected peer: ", connection)
+    );
+    _libp2p.on("peer:discovery", (peerId) =>
+      console.log("Peer discovery: ", peerId)
+    );
+
+    _libp2p.pubsub.subscribe("chuj123");
+
+    _libp2p.pubsub.on("chuj123", (msg) => console.log({ msg }));
+
     setLibp2p(_libp2p);
   };
 
@@ -18,10 +36,29 @@ export default function Home() {
       <>
         Not connected
         <br />
-        <button onClick={connect}>Connect</button>
+        <button
+          onClick={async () => {
+            try {
+              await connect();
+            } catch (e) {
+              console.log("Error when connecting: ", e);
+              alert(e.message);
+            }
+          }}
+        >
+          Connect
+        </button>
       </>
     );
   }
+
+  const publishMsg = async () => {
+    const textEncoder = new TextEncoder();
+    const data: Uint8Array = textEncoder.encode(
+      `c: ${Math.round(Math.random())}`
+    );
+    const res = libp2p.pubsub.publish("chuj123", data);
+  };
 
   console.log(libp2p);
   return (
@@ -32,21 +69,10 @@ export default function Home() {
       </Head>
       Connected
       <br />
-      {/* <button onClick={() => console.log(IPFS.pubsub.peers("chuj123"))}>
-        Log list of peers
-      </button>
+      <button onClick={() => console.log(libp2p)}>Log list of peers</button>
       <br />
-      <button onClick={() => IPFS.pubsub.publish("chuj123", "hellothere")}>
-        Send message
-      </button>
+      <button onClick={publishMsg}>Publish message</button>
       <br />
-      <button
-        onClick={() =>
-          IPFS.pubsub.subscribe("chuj123", (msg) => console.log({ msg }))
-        }
-      >
-        Subscribe
-      </button> */}
       <br />
       ID: {ID}
       <br />
