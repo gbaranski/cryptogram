9ac8bbd4293aea444ba960151c6a2ee887df69b6
gbaranski <gbaranski19@gmail.com>
2020-12-14 17:32:29 +0100

    refactor(cli): move withColor out of misc

--- cli/chat/ui.go
+++ cli/chat/ui.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"strings"
 
-	"github.com/gbaranski/cryptogram/cli/misc"
 	"github.com/gdamore/tcell/v2"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/rivo/tview"
@@ -146,7 +145,7 @@ func (ui *UI) displayChatMessage(cm *Message) {
 	if len(cm.Sender.Nickname) < 1 {
 		cm.Sender.Nickname = "Unknown"
 	}
-	prompt := misc.WithColor(color, fmt.Sprintf("<%s>:", cm.Sender.Nickname))
+	prompt := withColor(color, fmt.Sprintf("<%s>:", cm.Sender.Nickname))
 	fmt.Fprintf(ui.msgView, "%s %s\n", prompt, cm.Text)
 }
 
@@ -160,7 +159,7 @@ func (ui *UI) displayPeerEvent(e *pubsub.PeerEvent) {
 }
 
 func (ui *UI) printSystemMessage(args ...interface{}) {
-	prompt := misc.WithColor("red", "<System>:")
+	prompt := withColor("red", "<System>:")
 	fmt.Fprintln(ui.msgView, prompt, fmt.Sprint(args...))
 }
 
@@ -218,4 +217,9 @@ func (ui *UI) handleEvents() {
 			return
 		}
 	}
+}
+
+// WithColor wraps a string with color tags for display in the messages text box. Only for UI
+func withColor(color, msg string) string {
+	return fmt.Sprintf("[%s]%s[-]", color, msg)
 }
--- cli/misc/misc.go
+++
@@ -1,8 +0,0 @@
-package misc
-
-import "fmt"
-
-// WithColor wraps a string with color tags for display in the messages text box.
-func WithColor(color, msg string) string {
-	return fmt.Sprintf("[%s]%s[-]", color, msg)
-}
