2118c5bdfc0bdd657ead877a063bea39e51c11ae
gbaranski <gbaranski19@gmail.com>
2020-12-14 12:00:30 +0100

    feat(cli): parsing username from $USER

--- cli/misc/config.go
+++ cli/misc/config.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/Pallinder/go-randomdata"
-
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	maddr "github.com/multiformats/go-multiaddr"
 )
@@ -27,7 +26,7 @@ type DHTDiscoveryConfig struct {
 // Config configuration
 type Config struct {
 	// Unique string to identify group of nodes. Share this with your friends to let them connect with you
-	RendezvousString string
+	RendezvousString *string
 	Nickname         *string
 	ListenAddresses  *[]maddr.Multiaddr
 	ProtocolID       string
@@ -38,9 +37,17 @@ type Config struct {
 // GetConfig retreives config
 // Can exit the program
 func GetConfig() *Config {
-	nickname := flag.String("nick", "Generated in runtime", "Nickname")
+	usrEnv, usrEnvExists := os.LookupEnv("USER")
+	var nickname *string
+	if usrEnvExists {
+		nickname = flag.String("nick", usrEnv, "Nickname to identify yourself")
+	} else {
+		nickname = flag.String("nick", "Randomly generated", "Nickname to identify yourself")
+	}
+
 	dhtEnabled := flag.Bool("dht", false, "True if DHT discovery should be enabled")
 	mdnsEnabled := flag.Bool("mdns", false, "True if MDNS discovery should be enabled")
+	rendezvousString := flag.String("rendezvous", "cryptogram-rendezvous", "Unique string to identify group of nodes. Share this with your friends to let them connect with you")
 	flag.Parse()
 
 	if *dhtEnabled == false && *mdnsEnabled == false {
@@ -48,16 +55,15 @@ func GetConfig() *Config {
 		fmt.Println("Please run with `-dht` or/and `-mdns` option")
 		os.Exit(1)
 	}
-	// fix that stupid code later
-	if *nickname == "Generated in runtime" {
-		*nickname = fmt.Sprintf("%s-%s", randomdata.FirstName(randomdata.RandomGender), randomdata.LastName())
+	if !usrEnvExists {
+		randomNickname := fmt.Sprintf("%s-%s", randomdata.FirstName(randomdata.RandomGender), randomdata.LastName())
+		nickname = &randomNickname
 	}
 
 	config := &Config{
-		RendezvousString: "cryptogram-rendezvous",
+		RendezvousString: rendezvousString,
 		Nickname:         nickname,
 		ListenAddresses:  nil,
-		ProtocolID:       "/chat/1.0.0",
 		MDNSDiscovery: &MDNSDiscoveryConfig{
 			Enabled:  *mdnsEnabled,
 			Interval: time.Minute * 15,
