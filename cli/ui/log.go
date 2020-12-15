18d0d9a23cd582c0536b1e4b8d4ba5cba1475093
gbaranski <gbaranski19@gmail.com>
2020-12-15 18:02:13 +0100

    feat(cli): Add debug logging

--- cli/ui/log.go
+++ cli/ui/log.go
@@ -20,6 +20,15 @@ func (ui *UI) LogError(when string, err error) {
 	fmt.Fprintln(ui.msgView, fmt.Sprintf("%s Error occured when %s: %s", prompt, when, err.Error()))
 }
 
+// LogDebug similar to Log but logs only if debug mode
+func (ui *UI) LogDebug(args ...interface{}) {
+	if !*ui.config.Debug {
+		return
+	}
+	prompt := misc.WithColor("blue", "<Debug>:")
+	fmt.Fprintln(ui.msgView, prompt, fmt.Sprint(args...))
+}
+
 // displayChatMessage writes a ChatMessage from the room to the message window,
 // with the sender's nick highlighted in green.
 func (ui *UI) logChatMessage(cm *chat.Message) {
