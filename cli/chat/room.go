d4f124fffad076ec85430816120271a864413098
gbaranski <gbaranski19@gmail.com>
2020-12-19 19:57:40 +0100

    fix(cli): instead of panicking on invalid msg, continue loop

--- cli/chat/room.go
+++ cli/chat/room.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 
 	"github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
@@ -68,7 +67,7 @@ func (room *Room) readMessagesLoop(peerID *peer.ID) {
 		cm := new(Message)
 		err = json.Unmarshal(msg.Data, cm)
 		if err != nil {
-			log.Panicln("Failed unmarshalling message", err)
+			continue
 		}
 		// send valid messages onto the Messages channel
 		room.MsgCh <- cm
